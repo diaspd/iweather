37c0c50410424f74ac62df54071a2a5a0cff2ef9
Pedro Dias <pdr050203@gmail.com>
2024-12-09 22:41:05 -0300

    refactor: trade 'test' for 'it' on test

--- src/utils/getNextDays.test.ts
+++ src/utils/getNextDays.test.ts
@@ -1,6 +1,6 @@
 import { getNextDays } from "./getNextDays";
 
-test("should return the next five days", () => {
+it("should return the next five days", () => {
   const days = getNextDays();
 
   expect(days.length).toBe(5);
