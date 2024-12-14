e74da7c5576c9d4262f60adfdfaa4e5cf7db9d58
Pedro Dias <pdr050203@gmail.com>
2024-12-14 01:50:24 -0300

    test: add debug

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -4,6 +4,7 @@ import { Input } from '@components/Input';
 
 describe("Component: Input", () => {
   it("should be rendered without activity indicator", () => {
-    render(<Input />);
+    const { debug } = render(<Input />);
+    debug();
   })
 })
