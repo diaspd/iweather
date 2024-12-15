3bac6247ac01486457b68f5ffd43c8a065d37539
Pedro Dias <pdr050203@gmail.com>
2024-12-14 23:26:33 -0300

    test: add testId to input component

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -1,10 +1,12 @@
-import { render } from '@testing-library/react-native';
+import { render, screen } from '@testing-library/react-native';
 
 import { Input } from '@components/Input';
 
 describe("Component: Input", () => {
   it("should be rendered without activity indicator", () => {
-    const { debug } = render(<Input />);
-    debug();
+    render(<Input isLoading />);
+
+    const activityIndicator = screen.getByTestId("activity-indicator")
+    console.log(activityIndicator)
   })
 })
