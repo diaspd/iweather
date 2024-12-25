308b861cb3b08cae9bbe20cfe139584c7a68fd46
Pedro Dias <pdr050203@gmail.com>
2024-12-25 19:49:24 -0300

    test: add test to api function get weather by city

---
+++ src/services/getWeatherByCityService.test.ts
@@ -0,0 +1,14 @@
+import { api } from './api'
+
+import { mockWeatherAPIResponse } from '@__tests__/mocks/mockWeatherAPIResponse'
+import { getWeatherByCityService } from './getWeatherByCityService'
+
+describe("Service: getWeatherByCityService", () => {
+  it('should return weather api data formatted', async () => {
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockWeatherAPIResponse })
+
+    const response = await getWeatherByCityService({latitude: 123, longitude: 456})
+    
+    expect(response).toHaveProperty('today')
+  })
+})
