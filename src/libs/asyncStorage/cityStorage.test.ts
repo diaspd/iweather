25936c63191e10be72960df07401103eb5b5cf73
Pedro Dias <pdr050203@gmail.com>
2025-01-07 00:57:33 -0300

    test: verify if city is stored

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -1,8 +1,24 @@
-import { getStorageCity } from "@libs/asyncStorage/cityStorage";
+import { getStorageCity, saveStorageCity } from "@libs/asyncStorage/cityStorage";
+
+import type { CityProps } from "@services/getCityByNameService";
 
 describe("Storage: CityStorage", () => {
   it("should be able to return null when there's no city on storage", async () => {
     const response = await getStorageCity();
     expect(response).toBeNull();
+  });
+
+  it("should be able to return city storeged.", async () => {
+    const newCity: CityProps = {
+      id: '1',
+      name: 'São Paulo',
+      latitude: 123,
+      longitude: 456
+    }
+
+    await saveStorageCity(newCity)
+    const response = await getStorageCity();
+
+    expect(response).toEqual(newCity);
   })
 })
