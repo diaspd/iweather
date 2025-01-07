f592e61e1434646db02106aa291080e63512d855
Pedro Dias <pdr050203@gmail.com>
2025-01-07 01:01:32 -0300

    test: verify if was city removed

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -1,6 +1,13 @@
-import { getStorageCity, saveStorageCity } from "@libs/asyncStorage/cityStorage";
+import { getStorageCity, saveStorageCity, removeStorageCity } from "@libs/asyncStorage/cityStorage";
 
 import type { CityProps } from "@services/getCityByNameService";
+
+const newCity: CityProps = {
+  id: '1',
+  name: 'São Paulo',
+  latitude: 123,
+  longitude: 456
+}
 
 describe("Storage: CityStorage", () => {
   it("should be able to return null when there's no city on storage", async () => {
@@ -8,17 +15,19 @@ describe("Storage: CityStorage", () => {
     expect(response).toBeNull();
   });
 
-  it("should be able to return city storeged.", async () => {
-    const newCity: CityProps = {
-      id: '1',
-      name: 'São Paulo',
-      latitude: 123,
-      longitude: 456
-    }
-
+  it("should be able to return city stored.", async () => {
     await saveStorageCity(newCity)
     const response = await getStorageCity();
 
     expect(response).toEqual(newCity);
+  });
+
+  it("should be able to remove city from storage.", async () => {
+    await saveStorageCity(newCity);
+    await removeStorageCity();
+
+    const response = await getStorageCity();
+    expect(response).toBeNull();
   })
+
 })
