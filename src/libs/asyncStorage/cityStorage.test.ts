a61484ff396c9826c34ab5d3fee62dce2650f684
Pedro Dias <pdr050203@gmail.com>
2025-01-06 23:01:13 -0300

    test: finish city storage test

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -3,6 +3,6 @@ import { getStorageCity } from "@libs/asyncStorage/cityStorage";
 describe("Storage: CityStorage", () => {
   it("should be able to return null when there's no city on storage", async () => {
     const response = await getStorageCity();
-    console.log(response)
+    expect(response).toBeNull();
   })
 })
