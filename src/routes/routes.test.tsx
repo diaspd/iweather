12e66c200983b5641df27d8bcab97c70242457e9
Pedro Dias <pdr050203@gmail.com>
2025-01-09 02:28:53 -0300

    test: finish routes test by adding mock to api req

--- src/routes/routes.test.tsx
+++ src/routes/routes.test.tsx
@@ -1,8 +1,10 @@
-import { screen, waitFor } from "@testing-library/react-native"
+import { act, screen, waitFor } from "@testing-library/react-native"
 
 import { saveStorageCity } from "@libs/asyncStorage/cityStorage"
 import { render } from "@__tests__/utils/customRender"
+import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherAPIResponse"
 import { Routes } from "@routes/index"
+import { api } from "@services/api"
 
 describe("Routes", () => {
   it('should be able to render Search screen when not city selected.', async () => {
@@ -16,6 +18,8 @@ describe("Routes", () => {
   });
 
   it('shoul be able to render Dashboard screen when has city selected.', async () => {
+    jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse })
+    
     const city = {
       id: '1',
       name: 'São Paulo',
@@ -24,7 +28,10 @@ describe("Routes", () => {
     }
     
     await saveStorageCity(city)
-    const { debug } = render(<Routes />)
-    debug()
+
+    await act(() => waitFor(() => render(<Routes />)))
+    const title = screen.getByText(city.name)
+    
+    expect(title).toBeTruthy()
   })
 })
