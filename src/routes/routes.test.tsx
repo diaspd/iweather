1ff6c5baa7bd63512ed30da90c81e5e17c302561
Pedro Dias <pdr050203@gmail.com>
2025-01-09 02:27:00 -0300

    test: add base to render components wrapped in context

--- src/routes/routes.test.tsx
+++ src/routes/routes.test.tsx
@@ -1,5 +1,7 @@
-import { render, screen, waitFor } from "@testing-library/react-native"
+import { screen, waitFor } from "@testing-library/react-native"
 
+import { saveStorageCity } from "@libs/asyncStorage/cityStorage"
+import { render } from "@__tests__/utils/customRender"
 import { Routes } from "@routes/index"
 
 describe("Routes", () => {
@@ -11,5 +13,18 @@ describe("Routes", () => {
     const title = await waitFor(() => screen.findByText(/^escolha um local/i))
   
     expect(title).toBeTruthy()
+  });
+
+  it('shoul be able to render Dashboard screen when has city selected.', async () => {
+    const city = {
+      id: '1',
+      name: 'São Paulo',
+      latitude: 123,
+      longitude: 456
+    }
+    
+    await saveStorageCity(city)
+    const { debug } = render(<Routes />)
+    debug()
   })
 })
