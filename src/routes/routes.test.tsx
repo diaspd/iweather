7f635007720d787b800e5dede8ab5fd6ad1edd82
Pedro Dias <pdr050203@gmail.com>
2025-01-09 02:21:59 -0300

    test: add feat to find by text on route test

--- src/routes/routes.test.tsx
+++ src/routes/routes.test.tsx
@@ -1,12 +1,15 @@
-import { render } from "@testing-library/react-native"
+import { render, screen, waitFor } from "@testing-library/react-native"
 
 import { Routes } from "@routes/index"
 
 describe("Routes", () => {
-  it('should be render Search screen when not city selecte', () => {
-    const { debug } = render(
+  it('should be able to render Search screen when not city selected.', async () => {
+    render(
       <Routes />
     )
-    debug()
+
+    const title = await waitFor(() => screen.findByText(/^escolha um local/i))
+  
+    expect(title).toBeTruthy()
   })
 })
