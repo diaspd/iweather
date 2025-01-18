86fa4b80c53507c83efab96ee9c2d0eb606ab49c
Pedro Dias <pdr050203@gmail.com>
2025-01-18 01:55:27 -0300

    fix: test error

--- src/screens/Dashboard/dashboard.test.tsx
+++ src/screens/Dashboard/dashboard.test.tsx
@@ -1,13 +1,12 @@
 import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherAPIResponse"
 import { act, fireEvent, render, screen, waitFor, waitForElementToBeRemoved } from "@__tests__/utils/customRender"
-
-import { saveStorageCity } from "@libs/asyncStorage/cityStorage"
-
 import { api } from "@services/api"
 import { Dashboard } from "@screens/Dashboard"
+import { saveStorageCity } from "@libs/asyncStorage/cityStorage"
 import { mockCityAPIResponse } from "@__tests__/mocks/api/mockCityAPIResponse"
 
 describe("Screen: Dashboard", () => {
+
   beforeAll(async () => {
     const city = {
       id: '1',
@@ -18,35 +17,37 @@ describe("Screen: Dashboard", () => {
 
     await saveStorageCity(city)
   })
-  it('should be able to show city weather.', async () => {
+
+  it('should be show city weather', async () => {
     jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse });
-    
+
     render(<Dashboard />)
 
     await waitFor(() => expect(screen.findByText(/rio do sul/i, {}, { timeout: 3000 })).toBeTruthy);
   })
 
-  it("should be able to show another selected weather city.", async () => {
+  it('should be show another selected weather city', async () =>{
+
     jest.spyOn(api, 'get')
       .mockResolvedValueOnce({ data: mockWeatherAPIResponse })
       .mockResolvedValueOnce({ data: mockCityAPIResponse })
       .mockResolvedValueOnce({ data: mockWeatherAPIResponse })
 
-      render(<Dashboard />)
-      
-      await waitForElementToBeRemoved(() => screen.queryByTestId('loading'))
+    render(<Dashboard />)
 
-      const cityName = 'São Paulo'
+    await waitForElementToBeRemoved(() => screen.queryByTestId('loading'))
 
-      await waitFor(() => act(() => {
-        const search = screen.getByTestId('search-input')
-        fireEvent.changeText(search, cityName)
-      }))
+    const cityName = 'São Paulo'
 
-      await waitFor(() => act(() => {
-        fireEvent.press(screen.getByText(cityName, { exact: false }))
-      }))
+    await waitFor(() => act(() => {
+      const search = screen.getByTestId('search-input')
+      fireEvent.changeText(search, cityName)
+    }))
 
-      expect(screen.getByText(cityName, { exact: false })).toBeTruthy()
+    await waitFor(() => act(() => {
+      fireEvent.press(screen.getByText(cityName, { exact: false }))
+    }))
+
+    expect(screen.getByText(cityName, { exact: false })).toBeTruthy()
   })
 })
